29671077cae61413c8227331821b87097bc55bc7
Semyon Kirekov <semyon@kirekov.com>
2022-01-21 21:08:43 +0300

    add ServerTest

---
+++ src/test/java/com/example/demo/ServerTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ServerTest extends AbstractServerTest {
+
+  @Test
+  void test() {
+    String response = sendMessage("request");
+    assertEquals("request", response);
+  }
+
+  @Test
+  void test2() {
+    String response = sendMessage("request2");
+    assertEquals("request2", response);
+  }
+}
