113d8cc08aac26fd57b32ce2e550684a0089c2ae
fedrfedra <vetervell@gmail.com>
2022-01-22 13:42:11 +0200

    mild fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+    System.out.println("Hello, world!");
   }
 
 }
