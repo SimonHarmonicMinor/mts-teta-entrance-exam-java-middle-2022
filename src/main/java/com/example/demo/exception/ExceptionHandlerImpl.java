5977a3a78b0d8745a347d062678f198be591793a
VCKazakova <v.c.kazakova@gmail.com>
2022-01-29 11:26:33 +0300

    mts-teta-kazakova добавлен метод handle

--- src/main/java/com/example/demo/exception/ExceptionHandlerImpl.java
+++ src/main/java/com/example/demo/exception/ExceptionHandlerImpl.java
@@ -2,15 +2,21 @@ package com.example.demo.exception;
 
 import com.example.demo.entity.Result;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ExceptionHandlerImpl implements ExceptionHandler {
 
-    private Map<String, Result> resultMap;
+    private Map<String, Result> resultMap = new HashMap<>();
 
+    {
+        resultMap.put(FormatException.class.getCanonicalName(), Result.WRONG_FORMAT);
+        resultMap.put(RightException.class.getCanonicalName(), Result.ACCESS_DENIED);
+        resultMap.put(Error.class.getCanonicalName(), Result.ERROR);
+    }
 
     @Override
     public String handle(Exception e) {
-        return null;
+        return resultMap.getOrDefault(e.getClass().getCanonicalName(), Result.ERROR).name();
     }
 }
--- src/main/java/com/example/demo/service/specificCheckers/RightChecker.java
+++ src/main/java/com/example/demo/service/specificCheckers/RightChecker.java
@@ -2,6 +2,7 @@ package com.example.demo.service.specificCheckers;
 
 import com.example.demo.entity.Command;
 import com.example.demo.entity.Request;
+import com.example.demo.exception.RightException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.RequestChecker;
 
@@ -21,7 +22,7 @@ public class RightChecker implements RequestChecker {
         if (currentCommand.equals(Command.CLOSE_TASK) || currentCommand.equals(Command.DELETE_TASK) || currentCommand.equals(Command.REOPEN_TASK)) {
             for (String taskName : taskList) {
                 if (!taskName.equals(request.getAdditionalParam())) {
-                    throw new RuntimeException("Нет прав на совершение действия");
+                    throw new RightException("Нет прав на совершение действия");
                 }
             }
         }
