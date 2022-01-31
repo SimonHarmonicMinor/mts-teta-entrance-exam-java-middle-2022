626111f189b424fc564eaa55dc0f9e8f64ecbb66
VCKazakova <v.c.kazakova@gmail.com>
2022-01-31 11:54:03 +0300

    mts-teta-kazakova добавлен TaskCheckerTest

--- src/test/java/com/example/demo/service/specificCheckers/RightCheckerTest.java
+++ src/test/java/com/example/demo/service/specificCheckers/RightCheckerTest.java
@@ -2,7 +2,6 @@ package com.example.demo.service.specificCheckers;
 
 import com.example.demo.entity.Command;
 import com.example.demo.entity.Request;
-import com.example.demo.exception.FormatException;
 import com.example.demo.exception.RightException;
 import com.example.demo.repository.TaskRepository;
 import com.example.demo.repository.TaskRepositoryImpl;
@@ -12,7 +11,8 @@ import com.example.demo.service.commandExecutor.CreateExecute;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Тест должен:")
 class RightCheckerTest {
@@ -30,7 +30,7 @@ class RightCheckerTest {
 
     @Test
     @DisplayName("выбрасывать исключение RightException")
-    void checkARightThrowsRightExceptionTest() throws RightException {
+    void checkRightThrowsRightExceptionTest() throws RightException {
         new CreateExecute(taskRepository,userRepository).execute(new Request("DASHA",Command.CREATE_TASK, "Task4"));
         Request request = new Request("SVETA", Command.CLOSE_TASK, "Task4");
         assertThrows(RightException.class, () -> new RightChecker(taskRepository).check(request));
---
+++ src/test/java/com/example/demo/service/specificCheckers/TaskCheckerTest.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.specificCheckers;
+
+import com.example.demo.entity.Command;
+import com.example.demo.entity.Request;
+import com.example.demo.entity.Status;
+import com.example.demo.entity.Task;
+import com.example.demo.exception.FormatException;
+import com.example.demo.repository.TaskRepository;
+import com.example.demo.repository.TaskRepositoryImpl;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.UserRepositoryImpl;
+import com.example.demo.service.commandExecutor.CreateExecute;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("Тест должен:")
+class TaskCheckerTest {
+
+    TaskRepository taskRepository = new TaskRepositoryImpl();
+    UserRepository userRepository = new UserRepositoryImpl();
+
+    @Test
+    @DisplayName("выполнять проверку наличия задачи в списке задач")
+    void checkTaskCheckerTest() {
+        taskRepository.addTask(new Task("Task5", Status.CREATED, "DASHA"));
+        Request request = new Request("DASHA", Command.CLOSE_TASK, "Task5");
+        assertDoesNotThrow(() -> new TaskChecker(userRepository).check(request));
+    }
+
+    @Test
+    @DisplayName("выбрасывать исключение FormatException")
+    void checkTaskCheckerExceptionTest() throws FormatException {
+        taskRepository.addTask(new Task("Task", Status.CREATED, "DASHA"));
+        Request request = new Request("DASHA", Command.CLOSE_TASK, "Task5");
+        assertThrows(FormatException.class, () -> new TaskChecker(userRepository).check(request));
+    }
+
+}
