f37638dfbd242d071be7e734e39c91639f136f78
Tanya Pryntsova <tpryntsova@mail.ru>
2022-01-31 22:38:03 +0300

    feature-taprynts: try to fix tests

--- src/test/java/com/example/demo/AbstractServerTest.java
+++ src/test/java/com/example/demo/AbstractServerTest.java
@@ -1,17 +1,17 @@
 package com.example.demo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 import com.example.demo.service.RequestHandler;
 import com.example.demo.service.impl.TaskServiceImpl;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import static org.mockito.Mockito.mock;
 
@@ -21,14 +21,17 @@ public class AbstractServerTest {
   private static PrintWriter out;
   private static BufferedReader in;
   private static Server server;
-  protected TaskServiceImpl taskService;
+  protected static TaskServiceImpl taskService;
+
+  @BeforeAll
+  static void beforeAll() throws Exception {
+    taskService = mock(TaskServiceImpl.class);
+    server = new Server(new RequestHandler(taskService));
+    server.start();
+  }
 
   @BeforeEach
   void beforeEach() throws Exception {
-    taskService = mock(TaskServiceImpl.class);
-    RequestHandler requestHandler = new RequestHandler(taskService);
-    server = new Server(requestHandler);
-    server.start();
     clientSocket = new Socket("localhost", 9090);
     out = new PrintWriter(clientSocket.getOutputStream(), true);
     in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -39,7 +42,6 @@ public class AbstractServerTest {
     in.close();
     out.close();
     clientSocket.close();
-    server.stop();
   }
 
   @AfterAll
