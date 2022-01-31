39c1b2033c25db6711a0bd4184045b7bf38d6d3e
VCKazakova <v.c.kazakova@gmail.com>
2022-01-31 08:53:11 +0300

    mts-teta-kazakova добавлен CreateExecuteTest

---
+++ src/test/java/com/example/demo/service/commandExecutor/CreateExecuteTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.service.commandExecutor;
+
+import com.example.demo.entity.Command;
+import com.example.demo.entity.Request;
+import com.example.demo.repository.TaskRepository;
+import com.example.demo.repository.TaskRepositoryImpl;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.UserRepositoryImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Тест должен:")
+class CreateExecuteTest {
+
+    TaskRepository taskRepository = new TaskRepositoryImpl();
+    UserRepository userRepository = new UserRepositoryImpl();
+
+    @Test
+    @DisplayName("выполнять команду создания задачи")
+    void createExecuteTest() {
+        Request request = new Request("VASYA", Command.CREATE_TASK, "GoShopping");
+        String result = new CreateExecute(taskRepository, userRepository).execute(request);
+        assertEquals("CREATED", result);
+    }
+
+}
